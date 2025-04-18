097a928f886bbc7556a4173a13901b57a61ed37b
Goliath02 <kevin.krueger241215@gmail.com>
2025-04-18 10:35:45 +0200

    Improve logging

--- bsc-registration-server/src/main/java/bsc_registration/RegistrationModule.java
+++ bsc-registration-server/src/main/java/bsc_registration/RegistrationModule.java
@@ -6,6 +6,8 @@ import bsc_registration.Utils.CsvUtil;
 import bsc_registration.dto.FormData;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSendException;
 import org.springframework.stereotype.Service;
@@ -24,15 +26,19 @@ public class RegistrationModule {
 
     final private CsvUtil csvUtil;
 
+    final Logger logger = LoggerFactory.getLogger(RegistrationModule.class);
+
     public void sendEmailToRegistration(final FormData formData, final List<MultipartFile> files) throws MessagingException, IOException {
 
         final var config = configLoader.loadConfig();
 
         final var csvFromFormData = csvUtil.createCsvFromFormData(formData);
+	    logger.info("CSV was created successfully: {}", csvFromFormData);
 
         final var registrationReceiver = config.registrationReceiver();
 
         emailService.sendMailToRegistration(registrationReceiver, csvFromFormData, files);
+        logger.info("Email was sent successfully to: {}", registrationReceiver);
     }
 
     public void sendEmailToCourseOwner(final FormData formData) throws MessagingException {
@@ -45,6 +51,7 @@ public class RegistrationModule {
 
     public void sendEmailToRegistratedUser(FormData formData) throws MessagingException, MailSendException {
         emailService.sendEmailToUser(formData.mainData().email(), formData);
+        logger.info("Email was sent successfully to: {}", formData.mainData().email());
     }
 
     public List<String> getCourses() {
