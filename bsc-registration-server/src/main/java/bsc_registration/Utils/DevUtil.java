95bfdfe832af350f6185fcb085d0f71ec408aac3
Goliath02 <kevin.krueger241215@gmail.com>
2025-05-26 22:49:54 +0200

    Add Dev Utl

---
+++ bsc-registration-server/src/main/java/bsc_registration/Utils/DevUtil.java
@@ -0,0 +1,34 @@
+package bsc_registration.Utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DevUtil {
+
+	@Value("${dev.email}")
+	private String devEmail;
+
+	public String getEmailFromConfig(final String email) {
+
+		if (email.isBlank() || email.equals("example@mail.com")) {
+			return devEmail;
+		}
+
+		return email;
+	}
+
+	public List<String> getEmailFromConfig(final List<String> emails) {
+
+		return emails.stream().map(email -> {
+
+			if (email.isBlank() || email.equals("example@mail.com")) {
+				return devEmail;
+			}
+
+			return email;
+		}).toList();
+	}
+}
