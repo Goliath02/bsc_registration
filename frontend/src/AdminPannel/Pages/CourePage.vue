d284e5de4fd83fb125a9899a322503713206606f
Goliath02 <kevin.krueger241215@gmail.com>
2025-07-13 14:35:55 +0200

    Add Course overview (without logic)

--- frontend/src/AdminPannel/Pages/CourePage.vue
+++ frontend/src/AdminPannel/Pages/CourePage.vue
@@ -1,24 +1,104 @@
 <script setup lang="ts">
-import { Button } from 'primevue'
+import {Button} from 'primevue'
 import CourseCard from "@/AdminPannel/components/CourseCard.vue";
+import {ref} from "vue";
+
+const courses = ref([
+    {
+        title: "Nichtschimmer Kurs",
+        imagePath: "/images/NonSwimPic.jpg",
+        trainer: "Kevin Krüger",
+        status: "Aktiv",
+        fromDate: "01.01.2022",
+        toDate: "02.01.2022",
+        totalUnits: 15,
+        unitsDone: 0,
+        place: "Firtz-Erler Bad",
+        participants: 0,
+        maxParticipants: 15
+    },
+    {
+        title: "Nichtschimmer Kurs",
+        imagePath: "/images/TeenSwimPic.jpg",
+        trainer: "Kevin Krüger",
+        status: "Aktiv",
+        fromDate: "01.01.2022",
+        toDate: "02.01.2022",
+        totalUnits: 15,
+        unitsDone: 0,
+        place: "Firtz-Erler Bad",
+        participants: 0,
+        maxParticipants: 15
+    },
+    {
+        title: "Nichtschimmer Kurs",
+        imagePath: "/images/MasterSwim.jpg",
+        trainer: "Kevin Krüger",
+        status: "Aktiv",
+        fromDate: "01.01.2022",
+        toDate: "02.01.2022",
+        totalUnits: 15,
+        unitsDone: 0,
+        place: "Firtz-Erler Bad",
+        participants: 0,
+        maxParticipants: 15
+    },
+    {
+        title: "Nichtschimmer Kurs",
+        imagePath: "/images/TeenSwimPic.jpg",
+        trainer: "Kevin Krüger",
+        status: "Aktiv",
+        fromDate: "01.01.2022",
+        toDate: "02.01.2022",
+        totalUnits: 15,
+        unitsDone: 0,
+        place: "Firtz-Erler Bad",
+        participants: 0,
+        maxParticipants: 15
+    },
+    {
+        title: "Nichtschimmer Kurs",
+        imagePath: "/images/TeenSwimPic.jpg",
+        trainer: "Kevin Krüger",
+        status: "Aktiv",
+        fromDate: "01.01.2022",
+        toDate: "02.01.2022",
+        totalUnits: 15,
+        unitsDone: 0,
+        place: "Firtz-Erler Bad",
+        participants: 0,
+        maxParticipants: 15
+    },
+]);
+
 </script>
 
 <template>
-  <div class="flex flex-col h-full">
-    <div class="flex justify-between mx-8 mt-4">
-      <h2 class="text-2xl font-bold">Courses</h2>
-      <Button label="Add Course" icon="pi pi-plus" />
-    </div>
+	<div class="flex flex-col h-full">
+		<div class="flex justify-between mx-8 mt-4">
+			<h2 class="text-2xl font-bold">Courses</h2>
+			<Button label="Add Course" icon="pi pi-plus"/>
+		</div>
 
-    <div class="flex-1 m-4 overflow-auto">
-      <div class="bg-bsc-gray rounded-lg h-full overflow-clip p-4">
+		<div class="flex-1 m-4 overflow-auto">
+			<div class="bg-bsc-gray overflow-scroll rounded-lg h-full p-4 flex flex-wrap gap-x-4 gap-y-4 justify-center items-center">
 
-        <CourseCard/>
-
-
-      </div>
-    </div>
-  </div>
+				<CourseCard v-for="course of courses"
+				            :title="course.title"
+				            :imagePath="course.imagePath"
+				            :trainer="course.trainer"
+				            :status="course.status"
+				            :fromDate="course.fromDate"
+				            :toDate="course.toDate"
+				            :totalUnits="course.totalUnits"
+				            :unitsDone="course.unitsDone"
+				            :place="course.place"
+				            :participants="course.participants"
+				            :maxParticipants="course.maxParticipants"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped>
--- frontend/src/AdminPannel/components/CourseCard.vue
+++ frontend/src/AdminPannel/components/CourseCard.vue
@@ -1,29 +1,80 @@
 <script setup lang="ts">
 import Card from "primevue/card";
-import Button from "primevue/button";
+import ProgressBar from "primevue/progressbar";
+
+const props = defineProps({
+	title: String,
+	imagePath: String,
+	trainer: String,
+	status: String,
+	fromDate: String,
+	toDate: String,
+	totalUnits: Number,
+	unitsDone: Number,
+	place: String,
+	participants: Number,
+	maxParticipants: Number
+});
 </script>
 
 <template>
 
-    <Card style="width: 25rem; overflow: hidden">
-      <template #header>
-        <div class="">test</div>
-      </template>
-      <template #title>Advanced Card</template>
-      <template #subtitle>Card subtitle</template>
-      <template #content>
-        <p class="m-0">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
-          quas!
-        </p>
-      </template>
-      <template #footer>
-        <div class="flex gap-4 mt-1">
-          <Button label="Cancel" severity="secondary" outlined class="w-full" />
-          <Button label="Save" class="w-full" />
-        </div>
-      </template>
-    </Card>
+		<Card style="width: 25rem; overflow: hidden; "
+		      class=" select-none h-min ">
+			<template #header>
+				<div :style="{ backgroundImage: `url(${imagePath})` }" class="h-[6em] bg-cover bg-center"/>
+			</template>
+			<template #title >
+				<div class="flex items-center justify-between px-2 py-1 hover:bg-[#262626] rounded-lg transition-colors cursor-pointer">
+					<h3 class="underline font-bold">{{title}}</h3>
+					<div class="flex items-center gap-2">
+						<p class="text-lg font-bold">{{status}}</p>
+						<div class="w-4 h-4 rounded-full bg-green-500"></div>
+					</div>
+
+				</div>
+
+			</template>
+			<template #subtitle>
+
+				<div class="flex items-center w-min px-2 gap-2 cursor-pointer hover:text-white transition-colors whitespace-nowrap">
+					<div class="pi pi-user"></div>
+					<div>{{trainer}}</div>
+				</div>
+
+			</template>
+			<template #content>
+				<div>
+
+					<div class="flex flex-col gap-2 px-2">
+						<div class="flex justify-between items-center font-bold">
+							<div>{{fromDate}}</div>
+							<div>{{toDate}}</div>
+						</div>
+
+						<div class="px-4 flex flex-col gap-1 align-center justify-center">
+							<ProgressBar :value="2" style="--p-progressbar-border-radius: 15px; --p-progressbar-label-color: transparent"/>
+							<h5 class="font-bold text-center">{{ unitsDone }}/{{ totalUnits }} Einheiten</h5>
+						</div>
+					</div>
+
+				</div>
+			</template>
+			<template #footer>
+				<div class="flex justify-between items-center font-bold px-2">
+
+					<div class="flex items-center gap-2 cursor-pointer border border-transparent hover:bg-[#262626] px-2 py-2 rounded-lg transition-colors">
+						<div style="font-size: 1.2em" class="pi pi-map-marker"></div>
+						<h5>{{place}}</h5>
+					</div>
+
+					<div class="flex items-center gap-2 font-bold cursor-pointer border border-transparent hover:bg-[#262626] px-2 py-2 rounded-lg transition-colors">
+						<div style="font-size: 1.2em" class="pi pi-users"></div>
+						<h5>{{ participants }}/ {{ maxParticipants }} Teilnehmer</h5>
+					</div>
+				</div>
+			</template>
+		</Card>
 
 </template>
 
