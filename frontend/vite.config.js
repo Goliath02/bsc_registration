de3044299166bc8bc3e4eac8779ed130f040b8b3
Goliath02 <kevin.krueger241215@gmail.com>
2025-07-14 22:58:28 +0200

    Add PrimeVue auto-import resolver and unplugin-vue-components integration in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,19 @@ import {fileURLToPath, URL} from "node:url";
 import tailwindcss from '@tailwindcss/vite'
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(),
-        tailwindcss()],
+        tailwindcss(),
+        Components({
+            resolvers: [
+                PrimeVueResolver()
+            ]
+        })
+    ],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
