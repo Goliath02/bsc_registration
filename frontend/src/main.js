4153dcdcc6876b2536030928cd6e0d112cb9c97c
Goliath02 <kevin.krueger241215@gmail.com>
2025-08-28 22:22:11 +0200

    Add Tanstack for easier fetching data

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ import { createPinia } from "pinia";
 import router from "@/router.js";
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
+import { VueQueryPlugin } from "@tanstack/vue-query";
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -17,5 +18,6 @@ app.use(PrimeVue, {
 });
 app.use(router);
 app.use(pinia);
+app.use(VueQueryPlugin);
 
 app.mount("#app");
