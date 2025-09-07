1623a6d32212cf979d3131605646b80be47f2b67
Goliath02 <118054442+Goliath02@users.noreply.github.com>
2025-09-07 12:05:22 +0200

    WIP course page

--- frontend/src/AdminPannel/Pages/CourseDetailsPage.vue
+++ frontend/src/AdminPannel/Pages/CourseDetailsPage.vue
@@ -6,18 +6,20 @@ import { useQuery } from "@tanstack/vue-query";
 import { formatDate } from "@/utils/dateUtil";
 import { ref } from "vue";
 import DeleteCourseDialog from "@/AdminPannel/components/DeleteCourseDialog.vue";
+import AddCourseDialog from "@/AdminPannel/components/AddCourseDialog.vue";
 
 const route = useRoute();
 
 const courseId = route.params.id;
 
-const isOpen = ref(false);
+const isDeleteDialogOpen = ref(false);
+const isChangeDialogOpen = ref(false);
 
 export type CourseDetails = {
   courseId: number
   courseName: string
   courseType: string
-  startDate: string
+  startDate: Date
   endDate: string
   numberOfMaxParticipants: number
   trainingUnits: number
@@ -57,7 +59,18 @@ const { data: courseDetails, isLoading } = useQuery<CourseDetails>({
 
 
   <main v-else class="flex flex-col max-h-screen h-[92vh] md:h-[98vh]">
-    <DeleteCourseDialog v-model="isOpen" />
+    <DeleteCourseDialog v-model="isDeleteDialogOpen" />
+    <AddCourseDialog v-model="isChangeDialogOpen"
+                     :is-change="true"
+                     :course-title="courseDetails.courseName"
+                     :course-type="courseDetails.courseType"
+                     :start-date="courseDetails.startDate"
+                     :training-units="courseDetails.trainingUnits"
+                     :max-participants="courseDetails.numberOfMaxParticipants"
+                     :course-status="courseDetails.courseStatus"
+                     :trainer="courseDetails.courseOwnerName"
+                     :place="courseDetails.placeName"
+    />
     <div class="flex justify-between mx-8 mt-4">
     </div>
 
@@ -69,8 +82,8 @@ const { data: courseDetails, isLoading } = useQuery<CourseDetails>({
         <h1 class="text-3xl font-bold">{{courseDetails.courseName}}</h1>
 
         <div class="flex gap-4">
-          <Button label="Ändern" icon="pi pi-pencil" />
-          <Button label="Löschen" icon="pi pi-trash" @click="isOpen = true" />
+          <Button label="Ändern" icon="pi pi-pencil" @click="isChangeDialogOpen = true" />
+          <Button label="Löschen" icon="pi pi-trash" @click="isDeleteDialogOpen = true" />
         </div>
       </div>
 
